396896af1332ef7cb00137c859b16162c9be07f5
Leong Chee Fai <leongcheefai0521@gmail.com>
2025-08-19 12:25:45 +0800

    test flutter homepage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Protein Tracker',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Protein Tracker'),
     );
   }
 }
@@ -105,6 +105,15 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
+            const Text('awesome idea, lets do it'),
+
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed!');
+              },
+              child: Text('Next'),
+            ),
+            const SizedBox(height: 20),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
