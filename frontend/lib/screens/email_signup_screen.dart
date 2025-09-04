5160a5501a45a4c1663b08ac11843d84b832f79b
Leong Chee Fai <leongcheefai0521@gmail.com>
2025-09-04 17:34:22 +0800

    fix: unused import

--- frontend/lib/screens/email_signup_screen.dart
+++ frontend/lib/screens/email_signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'email_login_screen.dart';
 import '../main.dart';
 
 class EmailSignupScreen extends StatefulWidget {
